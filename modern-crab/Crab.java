574164e421854c373111f7557a823fbc3ef5d019
Oliver D'Avino <6252915@edu.vaniercollege.qc.ca>
2024-09-12 14:21:07 -0400

    I edited a ZombieLobster game

--- modern-crab/Crab.java
+++ modern-crab/Crab.java
@@ -11,16 +11,39 @@ public class Crab extends Actor
     /* (World, Actor, GreenfootImage, Greenfoot and MouseInfo)*/
 
     /**
-     * Act - do whatever the Crab wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     * 
      */
     public void act()
     {
-        move(4);
+        moveAndTurn();
+        eat();
+        
+    }
+
+    /**
+     * Moving
+     */
+    public void moveAndTurn()
+    {
         if (Greenfoot.isKeyDown("left")) {
             turn(-3);
         }
         if (Greenfoot.isKeyDown("right")) {
             turn(3);
+        }
+    }
+
+    /**
+     * Act - do whatever the Crab wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void eat()
+    {
+        move(4);
+        Actor worm = getOneIntersectingObject(Worm.class);
+        if (worm != null) {
+            World world = getWorld();
+            world.removeObject(worm);
+            Greenfoot.playSound("eating.wav");
         }
     }
 }
--- modern-crab/CrabWorld.java
+++ modern-crab/CrabWorld.java
@@ -17,5 +17,43 @@ public class CrabWorld extends World
     public CrabWorld()
     {
         super(560, 560, 1);
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Crab crab =  new Crab();
+        addObject(crab, 163, 294);
+        Worm worm =  new Worm();
+        addObject(worm, 486, 218);
+        Worm worm2 =  new Worm();
+        addObject(worm2, 507, 313);
+        Worm worm3 =  new Worm();
+        addObject(worm3, 409, 120);
+        Worm worm4 =  new Worm();
+        addObject(worm4, 419, 380);
+        Worm worm5 =  new Worm();
+        addObject(worm5, 303, 502);
+        Worm worm6 =  new Worm();
+        addObject(worm6, 465, 488);
+        Worm worm7 =  new Worm();
+        addObject(worm7, 190, 48);
+        Worm worm8 =  new Worm();
+        addObject(worm8, 360, 36);
+        Worm worm9 =  new Worm();
+        addObject(worm9, 185, 168);
+        Worm worm10 =  new Worm();
+        addObject(worm10, 77, 496);
+        Worm worm11 =  new Worm();
+        addObject(worm11, 213, 436);
+        Worm worm12 =  new Worm();
+        addObject(worm12, 65, 122);
+        ZombieLobster zombieLobster =  new ZombieLobster();
+        addObject(zombieLobster, 529, 21);
+        crab.setLocation(15, 547);
     }
 }
